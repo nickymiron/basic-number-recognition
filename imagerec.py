c42f900c39121f5c4854348620fae4435594f88a
Nick Mironenko <nickmironenko@gmail.com>
2017-05-23 08:16:44 +0300

    Store image as arrays training data to file

--- imagerec.py
+++ imagerec.py
@@ -4,6 +4,26 @@ import matplotlib.pyplot as plt
 import time
 from statistics import mean
 
+def createExamples():
+    numberArrayExamples = open('numArEx.txt','a')
+    numbersWeHave = range(1,10)
+    for eachNum in numbersWeHave:
+        #print eachNum
+        for furtherNum in numbersWeHave:
+            # you could also literally add it *.1 and have it create
+            # an actual float, but, since in the end we are going
+            # to use it as a string, this way will work.
+            print(str(eachNum)+'.'+str(furtherNum))
+
+            imgFilePath = 'images/numbers/'+str(eachNum)+'.'+str(furtherNum)+'.png'
+            ei = Image.open(imgFilePath)
+            eiar = np.array(ei)
+            eiarl = str(eiar.tolist())
+
+            print(eiarl)
+            lineToWrite = str(eachNum)+'::'+eiarl+'\n'
+            numberArrayExamples.write(lineToWrite)
+
 def threshold(imageArray):
     balanceAr = []
     newAr = imageArray
